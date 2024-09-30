ec8ff60ccf10467f469d8d44bf85936d82c3143d
Esubalew197 <109427994+Esubalew197@users.noreply.github.com>
2024-09-30 22:12:21 +0300

    Add README file modify config.py

--- medaccess-backend/config.py
+++ medaccess-backend/config.py
@@ -1,17 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:460K@r2000@localhost/medaccessdb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
 
-
-
-
-# class Config:
-#     SECRET_KEY = 'your_secret_key'
-#     MYSQL_HOST = 'localhost'
-#     MYSQL_USER = 'Esubalew'
-#     MYSQL_PASSWORD = '350B@lew'
-#     MYSQL_DB = 'medAccessDB'
-#     MYSQL_CURSORCLASS = 'DictCursor'
