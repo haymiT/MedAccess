9f4bd5471827616c138fd19b8b159e8a9f5aa910
berekettena <tena21bereket@gmail.com>
2024-10-28 11:16:22 +0300

    model modified customer deleted, sell and sell_item model added

--- medaccess/app/__init__.py
+++ medaccess/app/__init__.py
@@ -25,22 +25,25 @@ def create_app():
     app.static_folder = 'static'
 
     # Register blueprints for each route (consumer, pharmacy, supplier, inventory)
-    from app.routes.consumer import consumer_bp
+    # from app.routes.consumer import consumer_bp
     from app.routes.pharmacy import pharmacy_bp
     from app.routes.supplier import supplier_bp
     from app.routes.inventory import inventory_bp
     from app.routes.user import user_bp
     from app.routes.medication import medication_bp
     from app.routes.order import order_bp
+    from app.routes.sell import sell_bp
+    
     
 
-    app.register_blueprint(consumer_bp)
+    # app.register_blueprint(consumer_bp)
     app.register_blueprint(pharmacy_bp)
     app.register_blueprint(supplier_bp)
     app.register_blueprint(inventory_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(medication_bp)
     app.register_blueprint(order_bp)
+    app.register_blueprint(sell_bp)
 
 
     return app
--- medaccess/app/models/__init__.py
+++ medaccess/app/models/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Import models
-from .consumer import Consumer
+# from .consumer import Consumer
 from .pharmacy import Pharmacy
 from .supplier import Supplier
 from .inventory import Inventory
 from .user import User
 from .medication import Medication
 from .order import Order
+from .sell import Sell
+from .sell_item import SellItem
