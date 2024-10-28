30d1ca5e948b031b0a7995b477e0d153426017de
berekettena <tena21bereket@gmail.com>
2024-10-28 17:45:37 +0300

    Remove medaccess-backend folder

--- medaccess-backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CORS_HEADERS = 'Content-Type'
-
