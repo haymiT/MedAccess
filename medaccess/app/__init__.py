4116673fe1707fa6876b4415d063daf5c19ffe8a
Dagim Wallelgne <108694236+Dagwal@users.noreply.github.com>
2024-10-30 17:52:50 +0300

    Update __init__.py

--- medaccess/app/__init__.py
+++ medaccess/app/__init__.py
@@ -1,21 +1,23 @@
-# app/__init__.py
 from flask import Flask
 from app.models import db
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    
+    # CORS Configuration with credentials and explicit methods allowed
+    cors = CORS(app, resources={r"/*": {"origins": "http://localhost:6445"}}, supports_credentials=True)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.secret_key = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-      # Initialize the database
+    # Initialize the database
     db.init_app(app)
 
     # Initialize Flask-Migrate
@@ -24,8 +26,7 @@ def create_app():
     # Set static folder
     app.static_folder = 'static'
 
-    # Register blueprints for each route (consumer, pharmacy, supplier, inventory)
-    # from app.routes.consumer import consumer_bp
+    # Register blueprints for each route
     from app.routes.pharmacy import pharmacy_bp
     from app.routes.supplier import supplier_bp
     from app.routes.inventory import inventory_bp
@@ -33,9 +34,7 @@ def create_app():
     from app.routes.medication import medication_bp
     from app.routes.order import order_bp
     from app.routes.sell import sell_bp
-    
 
-    # app.register_blueprint(consumer_bp)
     app.register_blueprint(pharmacy_bp)
     app.register_blueprint(supplier_bp)
     app.register_blueprint(inventory_bp)
@@ -44,5 +43,13 @@ def create_app():
     app.register_blueprint(order_bp)
     app.register_blueprint(sell_bp)
 
+    # Allow credentials in responses
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:6445')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        return response
 
     return app
